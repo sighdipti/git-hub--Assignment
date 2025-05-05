76cf9ec19afc28d5129aa45a405b445e8b8c241d
Dipti singh <157701267+sighdipti@users.noreply.github.com>
2025-05-05 21:57:32 +0530

    Add files via upload

---
+++ 1_Q.js
@@ -0,0 +1,25 @@
+/*
+Task-1: 
+   Initialize a Git repository
+
+Navigate to the project directory in your terminal.
+Run the command git init to initialize an empty Git repository.
+
+
+Task-2:
+   Create a script.js file with a simple function
+
+Create a new file named script.js in the project directory.
+Add a simple JavaScript function to the file, for 
+
+example*/
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+
+  /**
+   * Task-3: Add and commit changes
+
+          Use the command git add . to stage all the changes in the project directory.
+          Run the command git commit -m "Initial commit" to commit the changes with a descriptive message.
+   */
